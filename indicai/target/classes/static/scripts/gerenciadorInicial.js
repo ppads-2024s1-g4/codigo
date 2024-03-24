4765c413ee5d32b1b1eb9b26b9fd1f908685e1d9
jeffersoncdantas <jeffersoncdantas@yahoo.com>
2024-03-24 12:52:12 -0300

    Commit Docker File
    Co-Authored-By: Marina Miki Sinzato <64978999+marinasinzato@users.noreply.github.com>
    Co-Authored-By: Leonardo Medeiros Silva Aparicio <leomedeirosaparicio@gmail.com>

--- indicai/target/classes/static/scripts/gerenciadorInicial.js
+++
@@ -1,9 +0,0 @@
-var btnGerenciarFilmes = document.querySelector("#btnGerenciarFilmes");
-var sectionFilmes = document.querySelector("#sectionFilmes");
-var landing = document.querySelector("#landing");
-
-btnGerenciarFilmes.addEventListener("click", function(){
-landing.style.display = "none";
-sectionFilmes.style.display = "block";
-
-})
