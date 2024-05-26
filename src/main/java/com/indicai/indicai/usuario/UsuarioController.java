f7933f60e7e673538d6615d93cf4bec38f1520eb
jeffersoncdantas <jeffersoncdantas@yahoo.com>
2024-05-25 21:16:16 -0300

    Update UsuarioController.java

--- src/main/java/com/indicai/indicai/usuario/UsuarioController.java
+++ src/main/java/com/indicai/indicai/usuario/UsuarioController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,8 +35,7 @@ public class UsuarioController {
   }
 
   @GetMapping("/{id}")
-  @PreAuthorize("hasRole('ADMIN') OR (hasRole('EMPLOYEE') AND #id == authentication.principal.id)")
-  public ResponseEntity<UsuarioResponseDto> getUsuario(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDto> getUsuario(@PathVariable Long id) {
     Usuario user = usuarioService.buscarPorId(id);
     return ResponseEntity.ok(UsuarioMapper.toDto(user));
   }
@@ -49,7 +47,6 @@ public class UsuarioController {
   }
 
   @GetMapping
-  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<List<Usuario>> getUsuarios() {
     List<Usuario> users = usuarioService.buscarTodos();
     return ResponseEntity.ok(users);
